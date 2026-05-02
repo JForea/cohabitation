17f88acae1a46ea973783ea729f63d38f788fccd
JForea <ngrigorenko655@gmail.com>
2026-05-02 22:50:23 +0300

    fix(backend): fix filtering in buyings get request

--- backend/src/main/java/com/example/backend/services/BuyingService.java
+++ backend/src/main/java/com/example/backend/services/BuyingService.java
@@ -14,6 +14,7 @@ import com.example.backend.repositories.BuyingRepository;
 import com.example.backend.repositories.ProfileRepository;
 import com.example.backend.specifications.BuyingSpecifications;
 import com.example.backend.types.Role;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -126,7 +127,7 @@ public class BuyingService {
                 .and(BuyingSpecifications.byAssignedTo(assignedTo))
                 .and(BuyingSpecifications.byPublic(isPublic, user.getId()));
 
-        return buyingRepository.findAllByApartment_IdOrderByCategory(apartmentId, spec).stream().map(BuyingDto::new).toList();
+        return buyingRepository.findAll(spec, Sort.by("category")).stream().map(BuyingDto::new).toList();
     }
 
     public StatusResponse changeStatus(Integer apartmentId, User user, Long buyingId) {

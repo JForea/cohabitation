b1e41f4a4994733bb9ddbf2287aec075bee41cfa
JForea <ngrigorenko655@gmail.com>
2026-05-05 12:17:06 +0300

    feat(frontend): add neighbours points top list to profile page

---
+++ frontend/lib/features/profile/presentation/ui/widgets/list_tiles/neighbours_top_list_tile.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:frontend/features/profile/presentation/ui/widgets/indicators/points_indicator.dart';
+import 'package:frontend/shared/data/models/profile.dart';
+import 'package:frontend/shared/presentation/theme/app_colors.dart';
+import 'package:frontend/shared/presentation/ui/widgets/avatars/avatar.dart';
+
+class NeighboursTopListTile extends StatelessWidget {
+  const NeighboursTopListTile({
+    super.key,
+    required this.position,
+    required this.profile,
+    required this.isActiveUser,
+  });
+
+  final Profile profile;
+  final int position;
+  final bool isActiveUser;
+
+  Widget _getPlaceIndicator(BuildContext context) {
+    final svgSize = 20.0;
+
+    return switch (position) {
+      1 => SvgPicture.asset(
+        "assets/icons/top/first.svg",
+        width: svgSize,
+        height: svgSize,
+      ),
+      2 => SvgPicture.asset(
+        "assets/icons/top/second.svg",
+        width: svgSize,
+        height: svgSize,
+      ),
+      3 => SvgPicture.asset(
+        "assets/icons/top/third.svg",
+        width: svgSize,
+        height: svgSize,
+      ),
+      _ => SizedBox(
+        width: svgSize,
+        child: Text(
+          "#$position",
+          textAlign: .center,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSurfaceVariant,
+            fontSize: 10,
+            fontWeight: .w600,
+          ),
+        ),
+      ),
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: .symmetric(vertical: 8, horizontal: 15),
+      width: .infinity,
+      decoration: BoxDecoration(
+        color: isActiveUser ? AppColors.blue.withAlpha(37) : Colors.transparent,
+      ),
+      child: Row(
+        children: [
+          _getPlaceIndicator(context),
+          SizedBox(width: 16),
+          Avatar(name: profile.name, size: 32, color: profile.color),
+          SizedBox(width: 12),
+          Text(
+            "${profile.name}${isActiveUser ? " (вы)" : ""}",
+            style: TextStyle(
+              color: isActiveUser
+                  ? AppColors.blue
+                  : Theme.of(context).colorScheme.onSurface,
+              fontSize: 14,
+              fontWeight: .w500,
+            ),
+          ),
+          Spacer(),
+          PointsIndicator(pointsCount: profile.points),
+        ],
+      ),
+    );
+  }
+}
--- frontend/lib/features/profile/presentation/ui/widgets/tabs/profile_tab.dart
+++ frontend/lib/features/profile/presentation/ui/widgets/tabs/profile_tab.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/features/profile/presentation/ui/widgets/app_bars/profile_app_bar.dart';
+import 'package:frontend/features/profile/presentation/ui/widgets/lists/neighbours_top_list.dart';
 import 'package:frontend/shared/data/providers/auth_provider.dart';
+import 'package:frontend/shared/data/providers/neighbours_provider.dart';
+import 'package:frontend/shared/presentation/ui/widgets/wrappers/tab_wrapper.dart';
 
 class ProfileTab extends ConsumerWidget {
   const ProfileTab({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final profile = ref.read(authProvider).value!.user!.profile!;
+    final authState = ref.watch(authProvider);
+    final neighboursState = ref.watch(neighboursProvider);
 
-    return Column(children: [ProfileAppBar(profile: profile)]);
+    final profile = authState.value!.user!.profile!;
+
+    return Column(
+      children: [
+        ProfileAppBar(profile: profile),
+        TabWrapper(
+          floatingButtonExists: false,
+          appBarExists: true,
+          children: [
+            neighboursState.when(
+              data: (neighbours) => NeighboursTopList(
+                userProfile: profile,
+                neighbours: neighbours,
+              ),
+              error: (e, _) => Text("Произошла ошибка."),
+              loading: () => Center(child: CircularProgressIndicator()),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
